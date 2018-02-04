a8b9a392a83d1b1fa0c3f19a3e631f488e06f790
Christopher Hanna <cdhannaphone@gmail.com>
2018-02-04 13:56:15 -0500

    #adds a perf script for setting framerate

---
+++ CakeBaker/Assets/globals/Performance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Performance : MonoBehaviour {
+
+    public int TargetFramerate = 60;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        QualitySettings.vSyncCount = 0;
+
+        Application.targetFrameRate = TargetFramerate;
+	}
+}
